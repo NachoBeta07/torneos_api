fb8265cea1820612ff992de893269051d9956b7d
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2023-01-27 18:08:03 -0500

    commit4

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -4,7 +4,7 @@ import { getConnection } from "./../database/database";
 async function f_categoria_disciplinas(req, res) {
     const {id_disciplinas} = req.body;
     const connection = await getConnection();
-    const result = await connection.query("CALL `listar_disciplinas_torneo`(?);",id_disciplinas);
+    const result = await connection.query("CALL `filtrar_categoria_disciplinas`(?);",id_disciplinas);
     res.send(result);
 }
 
--- src/controllers/usr.controller.js
+++ src/controllers/usr.controller.js
@@ -12,18 +12,16 @@ async function login(req, res) {
 
     console.log(username, password);
     const result = await connection.query("CALL filtrar_usuario(?);", username);
-    console.log(result.length);
 
-    if (result.length === 0)
-        return res.status(400).send("Invalid username or password");
+    if (result[0].length === 0)
+        return res.send({token:"Invalid username or password"});
 
 
     const account = result[0][0];
 
-    console.log(account);
     const isValid = await bcrypt.compare(password, account.password_usuario);
 
-    if (!isValid) return res.status(400).send("Invalid username or password");
+    if (!isValid) return res.send({token:"Invalid username or password"});
 
 
     const token = jwt.sign(
@@ -36,5 +34,5 @@ async function login(req, res) {
 
 export const methods = {
     login
-
+    
 };
