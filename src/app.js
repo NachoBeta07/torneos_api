7fec4ca4fe43291a1cfac2cc3f28879c49310ef9
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2023-01-30 19:31:30 -0500

    commit5

--- src/app.js
+++ src/app.js
@@ -28,9 +28,6 @@ app.use("/api/disciplina",disciplinaR)
 app.use("/api/categoria",categoriaR)
 app.use("/api/participantes",partipantesR)
 
-
-const multer = require('multer');
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/');
